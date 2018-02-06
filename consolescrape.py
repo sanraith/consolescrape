252346e43a4f4e1693682061ecfdd1714439c79a
Soma Zsják <zsjaks@gmail.com>
2018-02-06 21:05:56 +0100

    Added display for latest changes and run.bat.

--- consolescrape.py
+++ consolescrape.py
@@ -1,7 +1,7 @@
-import logging
-import requests
 import datetime
+import logging
 import pickle
+import requests
 from collections import namedtuple
 from lxml import html
 
@@ -33,90 +33,108 @@ class Game:
 GameState = namedtuple("GameState", ["timestamp", "price", "in_stock"])
 
 
-def load_url(url, iscached):
-    if iscached == True:
-        raise NotImplementedError("Caching is not implemented.")
-    page = requests.get(url)
-    content = page.content
-    logging.info("Loded page from the internet.")
-    return content
+class Scraper:
+    def __init__(self):
+        self.time_now = datetime.datetime.now()
 
+    @staticmethod
+    def load_url(url, iscached):
+        if iscached == True:
+            raise NotImplementedError("Caching is not implemented.")
+        page = requests.get(url)
+        content = page.content
+        logging.info("Loded page from the internet.")
+        return content
 
-def trim_elem(elements):
-    return str(elements[0]).strip()
+    @staticmethod
+    def trim_elem(elements):
+        return str(elements[0]).strip()
 
+    def scrape_page(self, content):
+        states = {}
+        tree = html.fromstring(content)
+        cards = tree.xpath("//div[contains(@class, 'content')]//article[contains(@class, 'card')]")
+        for card in cards:
+            title = self.trim_elem(card.xpath(".//h3[@class='product-title']/a/text()"))
+            price = self.trim_elem(card.xpath(".//div[@class='price']/div[contains(@class, 'now')]/text()"))
+            in_stock = bool(card.xpath(".//li[contains(@class, 'stock-info') and contains(text(), 'Készleten')]"))
+            try:
+                price = int(price.replace(" ", ""))
+            except ValueError:
+                price = None
+            states[title] = GameState(self.time_now, price, in_stock)
+        return states
 
-def scrape_page(content):
-    states = {}
-    time_now = datetime.datetime.now()
-    tree = html.fromstring(content)
-    cards = tree.xpath("//div[contains(@class, 'content')]//article[contains(@class, 'card')]")
-    for card in cards:
-        title = trim_elem(card.xpath(".//h3[@class='product-title']/a/text()"))
-        price = trim_elem(card.xpath(".//div[@class='price']/div[contains(@class, 'now')]/text()"))
-        in_stock = bool(card.xpath(".//li[contains(@class, 'stock-info') and contains(text(), 'Készleten')]"))
+    def fetch_game_states(self, games_dict):
+        page_index = 0
+        while True:
+            page_index += 1
+            url = "https://www.konzolvilag.hu/switch/jatekok/oldal-{}".format(page_index)
+            print("Loading page: {}".format(url))
+            try:
+                content = self.load_url(url, iscached=False)
+                game_states = self.scrape_page(content)
+            except ValueError:
+                break
+            if not game_states:
+                print("Done.")
+                break
+
+            for title, game_state in game_states.items():
+                if title not in games_dict:
+                    games_dict[title] = Game(title)
+                games_dict[title].add_state(game_state)
+        return games_dict
+
+    def print_available_games(self, games):
+        available_games = list(filter(lambda x: x.state.price and x.state.in_stock, games))
+        max_title_length = max([len(x.title) for x in available_games])
+        max_price_length = max([len(str(x.state.price)) for x in available_games])
+        print("\nGames: {}, available: {}.".format(len(games), len(available_games)))
+        for game in sorted(available_games, key=lambda x: x.title):
+            padded_title = game.title.ljust(max_title_length)
+            padded_price = str(game.state.price).rjust(max_price_length)
+            print("{title} - {price} Ft [{timestamp}]".format(
+                title=padded_title, price=padded_price, timestamp=game.state.timestamp.strftime("%y.%m.%d. %H:%M")))
+
+    def print_last_changes(self, games):
+        last_date = max([x.state.timestamp for x in games])
+        print("\nLast changes [{}]:".format(last_date.strftime("%y.%m.%d. %H:%M")))
+        changed_games = sorted(filter(lambda x: x.state.timestamp == last_date, games), key=lambda x: x.title)
+        max_title_length = max([len(x.title) for x in changed_games])
+        for game in changed_games:
+            s1 = game.states[-2] if len(game.states) > 1 else game.state
+            s2 = game.state
+            stock_change = ""
+            price_change = ""
+            if s1.in_stock != s2.in_stock:
+                stock_change = "Now {}. ".format("available" if s2.in_stock else "unavailable")
+            if s1.price != s2.price:
+                price_change = "{} => {} Ft ".format(s1.price, s2.price)
+
+            state_str = "{n} - {a}{p}".format(n=game.title.ljust(max_title_length), p=price_change, a=stock_change)
+            print(state_str)
+
+    def load_db(self):
         try:
-            price = int(price.replace(" ", ""))
-        except ValueError:
-            price = None
-        states[title] = GameState(time_now, price, in_stock)
-    return states
+            with open("games.db", "rb") as fb:
+                games_dict = pickle.load(fb)
+        except FileNotFoundError:
+            games_dict = {}
+        return games_dict
 
-
-def fetch_game_states(games_dict):
-    page_index = 0
-    while True:
-        page_index += 1
-        url = "https://www.konzolvilag.hu/switch/jatekok/oldal-{}".format(page_index)
-        print("Loading page: {}".format(url))
-        try:
-            content = load_url(url, iscached=False)
-            game_states = scrape_page(content)
-        except ValueError:
-            break
-        if not game_states:
-            print("Done.\n")
-            break
-
-        for title, game_state in game_states.items():
-            if title not in games_dict:
-                games_dict[title] = Game(title)
-            games_dict[title].add_state(game_state)
-    return games_dict
-
-
-def print_available_games(games):
-    available_games = list(filter(lambda x: x.state.price and x.state.in_stock, games))
-    max_title_length = max([len(x.title) for x in available_games])
-    max_price_length = max([len(str(x.state.price)) for x in available_games])
-    last_change = max([x.state.timestamp for x in available_games])
-    for game in sorted(available_games, key=lambda x: x.title):
-        padded_title = game.title.ljust(max_title_length)
-        padded_price = str(game.state.price).rjust(max_price_length)
-        print("{title} {price} Ft [{timestamp}]".format(
-            title=padded_title, price=padded_price, timestamp=game.state.timestamp))
-    print("\nGames: {}, available: {}, last change: {}".format(len(games), len(available_games), last_change))
-
-
-def load_db():
-    try:
-        with open("games.db", "rb") as fb:
-            games_dict = pickle.load(fb)
-    except FileNotFoundError:
-        games_dict = {}
-    return games_dict
-
-
-def save_db(games_dict):
-    with open("games.db", "wb") as fb:
-        pickle.dump(games_dict, fb)
+    def save_db(self, games_dict):
+        with open("games.db", "wb") as fb:
+            pickle.dump(games_dict, fb)
 
 
 def main():
-    games_dict = load_db()
-    fetch_game_states(games_dict)
-    print_available_games(games_dict.values())
-    save_db(games_dict)
+    scraper = Scraper()
+    games_dict = scraper.load_db()
+    scraper.fetch_game_states(games_dict)
+    scraper.print_available_games(games_dict.values())
+    scraper.print_last_changes(games_dict.values())
+    scraper.save_db(games_dict)
 
 
 if __name__ == "__main__":
---
+++ run.bat
@@ -0,0 +1,2 @@
+@echo off
+cmd /k ".venv\Scripts\activate.bat & python consolescrape.py & .venv\Scripts\deactivate.bat & echo."
